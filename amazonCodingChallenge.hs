c55cf01690c71d90c51f29f60e2583dc9c01205b
CharlesWang <hcw320@ic.ac.uk>
2022-07-12 18:03:11 +0100

    Fix findPossibleMove by filtering out the out-bound moves

--- amazonCodingChallenge.hs
+++ amazonCodingChallenge.hs
@@ -5,8 +5,17 @@ equal :: Pos -> Pos -> Bool
 equal (x1, y1) (x2, y2) = x1 == x2 && y1 == y2
 
 -- Assume we only consider 10*10 grid
-findPossibleMove :: Pos ->  [Pos]
-findPossibleMove (x,y) = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+findAllMove :: Pos ->  [Pos]
+findAllMove (x,y) = [(x-1, y), (x+1, y), (x, y-1), (x, y+1), (x-1,y-1), (x+1,y-1), (x-1,y+1), (x+1,y+1)]
+
+findPossibleMove :: Pos -> [Pos]
+findPossibleMove pos = filter inBound (findAllMove pos)
+
+inBound :: Pos -> Bool
+inBound (x,y)
+    | x < 0 || x > 9 = False
+    | y < 0 || y > 9 = False
+    | otherwise = True
 
 findPath :: Pos -> Pos -> [Pos] -> [Pos] -> [Pos]
 findPath start@(x1,y1) dest@(x2,y2) obs steps
